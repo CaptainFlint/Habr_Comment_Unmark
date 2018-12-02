624994f2d677f89632c81d437843fff35dfe4291
Konstantin Vlasov <support@flint-inc.ru>
2018-12-02 03:36:20 +0300

    Adapted to the new 'today/yesterday' dates

--- Habr_Comment_Unmark.user.js
+++ Habr_Comment_Unmark.user.js
@@ -3,7 +3,7 @@
 // @description     Marks comments as unread
 // @author          Konstantin Vlasov (@CaptainFlint)
 // @namespace       Habrahabr
-// @version         1.0
+// @version         1.1
 // @include         https://habr.com/*
 // @grant           none
 // ==/UserScript==
@@ -15,15 +15,35 @@
 if (window != window.top)
 	return;
 
+function fmt2(v) {
+	return ((v > 9) ? '' : '0') + v;
+}
+
+// Convert date from a comment header into comparable string
+function translateDate(d) {
+	var parts = d.split(' в ');
+	if (parts[0] == 'сегодня') {
+		parts[0] = loadDate.getFullYear() + '-' + fmt2(loadDate.getMonth() + 1) + '-' + fmt2(loadDate.getDate());
+	}
+	else if (parts[0] == 'вчера') {
+		var yesterday = new Date(loadDate.getTime() - 24 * 3600 * 1000);
+		parts[0] = yesterday.getFullYear() + '-' + fmt2(yesterday.getMonth() + 1) + '-' + fmt2(yesterday.getDate());
+	}
+	else {
+		parts[0] = '20' + parts[0].split('.').reverse().join('-')
+	}
+	return parts.join(' ');
+}
+
+var loadDate = new Date();
+
 function mark_impl(dt) {
 	$('.comment__head_new-comment').removeClass('comment__head_new-comment');
 	$('.js-comment_new').removeClass('js-comment_new');
 
 	$(".js-comment")
 		.filter(function (index, el) {
-			var parts = $('> .comment > .comment__head > time', el).text().split(' в ');
-			parts[0] = '20' + parts[0].split('.').reverse().join('-')
-			return (parts.join(' ') >= dt);
+			return (translateDate($('> .comment > .comment__head > time', el).text()) >= dt);
 		})
 		.addClass('js-comment_new')
 		.find('> .comment > .comment__head')
@@ -34,12 +54,8 @@ function mark_impl(dt) {
 }
 
 win.addEventListener("load", function() {
-	function fmt2(v) {
-		return ((v > 9) ? '' : '0') + v;
-	}
-	var d = new Date();
-	var df = d.getFullYear() + '-' + fmt2(d.getMonth() + 1) + '-' + fmt2(d.getDate()) + ' ' + fmt2(d.getHours()) + ':' + fmt2(d.getMinutes());
-	$('.comments-section__subscribe-panel').prepend('<div style="font-size: smaller;">Развидеть: <input type="text" id="unmark_count" value="' + $('#xpanel > span.new').text() + '" style="width: 4em; text-align: right;" /> <input type="button" id="unmark_count_do" value="штук" /> или с момента <input type="text" id="unmark_date" value="' + df + '" style="width: 10em; text-align: right;" /> <input type="button" id="unmark_date_do" value="OK" /></div>');
+	var loadDateFmt = loadDate.getFullYear() + '-' + fmt2(loadDate.getMonth() + 1) + '-' + fmt2(loadDate.getDate()) + ' ' + fmt2(loadDate.getHours()) + ':' + fmt2(loadDate.getMinutes());
+	$('.comments-section__subscribe-panel').prepend('<div style="font-size: smaller;">Развидеть: <input type="text" id="unmark_count" value="' + $('#xpanel > span.new').text() + '" style="width: 4em; text-align: right;" /> <input type="button" id="unmark_count_do" value="штук" /> или с момента <input type="text" id="unmark_date" value="' + loadDateFmt + '" style="width: 10em; text-align: right;" /> <input type="button" id="unmark_date_do" value="OK" /></div>');
 	$('#unmark_date_do').on("click", function() {
 		var dt = $('#unmark_date').val();
 		mark_impl(dt);
@@ -48,9 +64,7 @@ win.addEventListener("load", function() {
 		var cnt = $('#unmark_count').val();
 		var timestamps = [];
 		$(".js-comment").each(function (index, el) {
-			var parts = $('> .comment > .comment__head > time', el).text().split(' в ');
-			parts[0] = '20' + parts[0].split('.').reverse().join('-')
-			timestamps.push(parts.join(' '));
+			timestamps.push(translateDate($('> .comment > .comment__head > time', el).text()));
 		});
 		timestamps.sort();
 		if (cnt > timestamps.length) {
